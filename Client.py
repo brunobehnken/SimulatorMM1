16ffda7339cb7781224dc56f7d353a47436639e8
brunobehnken <brunobehnken@yahoo.com.br>
2018-11-15 11:09:20 -0200

    Updates attributes with getters and setters

--- Client.py
+++ Client.py
@@ -6,6 +6,8 @@ class Client:
         """Sets the arrival time and the service time"""
         self.__arrival_time = arrival_time
         self.__service_time = service_time
+        self.__wait_time = None
+        self.__departure_time = None
 
     def get_arrival_time(self):
         """Returns the arrival time"""
@@ -14,3 +16,19 @@ class Client:
     def get_service_time(self):
         """Returns the service time"""
         return self.__service_time
+
+    def get_wait_time(self):
+        """Returns the wait time"""
+        return self.__wait_time
+
+    def set_wait_time(self, wait_time):
+        """Sets the wait time"""
+        self.__wait_time = wait_time
+
+    def get_departure_time(self):
+        """Returns the departure time"""
+        return self.__departure_time
+
+    def set_departure_time(self, departure_time):
+        """Sets the departure time"""
+        self.__departure_time = departure_time
