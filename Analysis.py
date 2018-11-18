eed1146c27376994eee65ec3ac3ef1be239f3cc0
brunobehnken <brunobehnken@yahoo.com.br>
2018-11-18 08:34:55 -0200

    Confidence interval functions generalized. set_best_k_for_variance() currently broken.

--- Analysis.py
+++ Analysis.py
@@ -1,3 +1,5 @@
+from scipy.stats import t, chi2, sqrt
+
 from Simulator import SimulatorFCFS, SimulatorLCFS
 import math
 import time
@@ -111,48 +113,61 @@ class Analysis:
         self.__waiting_times_variance, self.__areas_variance = \
             self.__calculate_samples_variances(rounds_means, self.__waiting_times_mean, self.__areas_mean)
 
-    def confidence_interval_for_waiting_time_mean(self):
-        """Calculates interval of confidence for the mean of the waiting time in line by the t-Student distribution"""
-        t_student_0975 = 1.960
-        upper = self.__waiting_times_mean + t_student_0975 * (
-            math.sqrt(self.__waiting_times_variance / self.__number_of_rounds))
-        lower = self.__waiting_times_mean - t_student_0975 * (
-            math.sqrt(self.__waiting_times_variance / self.__number_of_rounds))
+    def confidence_interval_for_mean(self, mean, variance, num_rounds, confidence_interval):
+        """Calculates interval of confidence for the mean by the t-Student distribution"""
+        alpha = 1 - confidence_interval
+        alpha /= 2
+        t_student = t.isf(alpha, num_rounds - 1)
+        mul = sqrt(variance / num_rounds)
+        upper = mean + t_student * mul
+        lower = mean - t_student * mul
+        # t_student_0975 = 1.960
+        # upper = self.__waiting_times_mean + t_student_0975 * (
+        #     math.sqrt(self.__waiting_times_variance / self.__number_of_rounds))
+        # lower = self.__waiting_times_mean - t_student_0975 * (
+        #     math.sqrt(self.__waiting_times_variance / self.__number_of_rounds))
         precision = (upper - lower) / (upper + lower)
 
         return upper, lower, precision
 
-    def confidence_interval_for_waiting_time_variance(self):
-        """Calculates interval of confidence for the variance
-        of the waiting time in line by the Chi-square distribution"""
-        chi_square_0975 = 3044.1302
-        chi_square_0025 = 3357.6582
-        upper = ((self.__number_of_rounds - 1) * self.__waiting_times_variance) / chi_square_0975
-        lower = ((self.__number_of_rounds - 1) * self.__waiting_times_variance) / chi_square_0025
+    def confidence_interval_for_variance(self, variance, num_rounds, confidence_interval):
+        """Calculates interval of confidence for the variance by the Chi-square distribution"""
+        alpha_lower = 1 - confidence_interval
+        alpha_lower /= 2
+        alpha_upper = 1 - alpha_lower
+        df = num_rounds - 1
+        chi2_lower = chi2.isf(alpha_lower, df)
+        chi2_upper = chi2.isf(alpha_upper, df)
+        lower = df * variance / chi2_lower
+        upper = df * variance / chi2_upper
+        # chi_square_0975 = 3044.1302
+        # chi_square_0025 = 3357.6582
+        # upper = ((self.__number_of_rounds - 1) * self.__waiting_times_variance) / chi_square_0975
+        # lower = ((self.__number_of_rounds - 1) * self.__waiting_times_variance) / chi_square_0025
         precision = (upper - lower) / (upper + lower)
 
         return upper, lower, precision
 
-    def confidence_interval_for_area_mean(self):
-        """Calculates interval of confidence for the mean
-        of the graphic's (number of people in line X time) area by the t-Student distribution"""
-        t_student_0975 = 1.960
-        upper = self.__areas_mean + t_student_0975 * (math.sqrt(self.__areas_variance / self.__number_of_rounds))
-        lower = self.__areas_mean - t_student_0975 * (math.sqrt(self.__areas_variance / self.__number_of_rounds))
-        precision = (upper - lower) / (upper + lower)
-
-        return upper, lower, precision
-
-    def confidence_interval_for_area_variance(self):
-        """Calculates interval of confidence for the variance
-        of the graphic's (number of people in line X time) area by the Chi-square distribution"""
-        chi_square_0975 = 3044.1302
-        chi_square_0025 = 3357.6582
-        upper = ((self.__number_of_rounds - 1) * self.__areas_variance) / chi_square_0975
-        lower = ((self.__number_of_rounds - 1) * self.__areas_variance) / chi_square_0025
-        precision = (upper - lower) / (upper + lower)
-
-        return upper, lower, precision
+    # def confidence_interval_for_area_mean(self):
+    #     """Calculates interval of confidence for the mean
+    #     of the graphic's (number of people in line X time) area by the t-Student distribution"""
+    #     t_student_0975 = 1.960
+    #     upper = self.__areas_mean + t_student_0975 * (math.sqrt(self.__areas_variance / self.__number_of_rounds))
+    #     lower = self.__areas_mean - t_student_0975 * (math.sqrt(self.__areas_variance / self.__number_of_rounds))
+    #     precision = (upper - lower) / (upper + lower)
+    #
+    #     return upper, lower, precision
+    #
+    # def confidence_interval_for_area_variance(self):
+    #     """Calculates interval of confidence for the variance
+    #     of the graphic's (number of people in line X time) area by the Chi-square distribution"""
+    #     chi_square_0975 = 3044.1302
+    #     chi_square_0025 = 3357.6582
+    #     upper = ((self.__number_of_rounds - 1) * self.__areas_variance) / chi_square_0975
+    #     lower = ((self.__number_of_rounds - 1) * self.__areas_variance) / chi_square_0025
+    #     precision = (upper - lower) / (upper + lower)
+    #
+    #     return upper, lower, precision
 
 
 def transient_phase_analysis():
@@ -221,7 +236,9 @@ def set_best_k_for_variance():
     analyzer.set_best_k_value(initial_k=10, k_rounds=10)
     print(analyzer.get_k())
     analyzer.set_stats_for_best_k_value()
-    waiting_time_mean_interval = analyzer.confidence_interval_for_waiting_time_mean()
+    waiting_time_mean_interval = analyzer.confidence_interval_for_waiting_time_mean(self.__waiting_times_mean,
+                                                                                    self.__waiting_times_variance,
+                                                                                    3200, 0.95)
     waiting_time_variance_interval = analyzer.confidence_interval_for_waiting_time_variance()
     print(waiting_time_mean_interval)
     print(waiting_time_variance_interval)
