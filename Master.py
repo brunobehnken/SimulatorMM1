b2ea417a1b4ae05fdc196f3dff9ecda77d44d48a
brunobehnken <brunobehnken@yahoo.com.br>
2018-11-18 18:24:43 -0200

    Transient phase removed to be redirected to method implemented in Simulator class

--- Master.py
+++ Master.py
@@ -4,9 +4,8 @@ import time
 
 class Master:
 
-    def __init__(self, number_of_rounds, transient_phase_length, rho):
+    def __init__(self, number_of_rounds, rho):
         self.__number_of_rounds = number_of_rounds
-        self.__transient_phase_length = transient_phase_length
         self.__simulator = SimulatorFCFS(rho)
         self.__waiting_times_mean = None
         self.__areas_mean = None
@@ -22,9 +21,6 @@ class Master:
 
     def set_k(self, k):
         self.__best_k_value = k
-
-    def __transient_phase(self):
-        self.__simulator.simulate_FCFS(self.__transient_phase_length)
 
     def __calculate_round_means(self, k):
         stats = self.__simulator.simulate_FCFS(k)
@@ -46,7 +42,7 @@ class Master:
         for _ in range(k_rounds):
             rounds_means = []  # list of round's means (waiting time mean and area mean)
             print("k: ", k)
-            self.__transient_phase()
+            self.__transient_phase()  # set to call method on Simulator
             for _round in range(self.__number_of_rounds):
                 if _round % 500 == 0:
                     print("Round: " + str(_round) + "   K : " + str(k))
@@ -82,7 +78,7 @@ class Master:
         """Sets the mean and variance of the waiting time in line and area of graphic(number of people in line X time)
         of all rounds. Each round having k samples collected, with the best value of k calculated in the function
         set_best_k_value(self, initial_k, k_rounds)"""
-        self.__transient_phase()
+        self.__transient_phase()  # set to call method on Simulator
         rounds_means = []
         for _round in range(self.__number_of_rounds):
             rounds_means.append(self.__calculate_round_means(self.__best_k_value))
@@ -91,68 +87,8 @@ class Master:
             self.__calculate_samples_variances(rounds_means, self.__waiting_times_mean, self.__areas_mean)
 
 
-def transient_phase_analysis():
-    analyzer = Master(number_of_rounds=3200, transient_phase_length=100, rho=0.8)
-    simulator = analyzer.get_simulator()
-    c2 = 0
-    consecutive = 0
-    consecutive_between = 0
-    sizes = []
-    sizes_between = []
-    lowest_variance = 10
-    for r in range(10):
-        res = simulator.simulate_FCFS(100000)
-        last = []
-        var_sum = 0
-        c = 0
-        last_was_less_than_lowest_variance = False
-        for i in res[1]:
-            last.append(i)
-            if len(last) > 1000:
-                last.pop(0)
-                mean = sum(last) / 1000
-                for e in last:
-                    var_sum += (e - mean) ** 2
-                variance = var_sum / (1000 - 1)
-                var_sum = 0
-                if variance <= lowest_variance:
-                    if last_was_less_than_lowest_variance:
-                        consecutive += 1
-                    else:
-                        if consecutive_between > 0:
-                            sizes_between.append(consecutive_between)
-                            consecutive_between = 0
-                    last_was_less_than_lowest_variance = True
-                else:
-                    if last_was_less_than_lowest_variance:
-                        if consecutive > 0:
-                            c2 += 1
-                            sizes.append(consecutive)
-                            consecutive = 0
-                    else:
-                        consecutive_between += 1
-                    last_was_less_than_lowest_variance = False
-
-            c += 1
-        consecutive = 0
-        consecutive_between = 0
-        if len(sizes) > 0:
-            print("count: " + str(c2) + "  size mean: " + str(sum(sizes) / len(sizes)))
-        else:
-            print("no equilibrium this time")
-        if len(sizes_between) > 0:
-            print("size between mean: ", sum(sizes_between) / len(sizes_between))
-        elif not (len(sizes_between) > 0) and not (len(sizes) > 0):
-            print("no equilibrium this time")
-        else:
-            print("always on equilibrium")  # this will probably never happen
-        sizes = []
-        sizes_between = []
-        c2 = 0
-
-
 def set_best_k_for_variance():
-    analyzer = Master(number_of_rounds=3200, transient_phase_length=30000, rho=0.8)
+    analyzer = Master(number_of_rounds=3200, rho=0.8)
 
     analyzer.set_best_k_value(initial_k=10, k_rounds=10)
     print(analyzer.get_k())
@@ -188,3 +124,63 @@ set_best_k_for_variance()
 
 finish = time.time() - start
 print("runtime: " + str(round(finish / 60, 2)) + " min")
+
+
+# def transient_phase_analysis():
+#     analyzer = Master(number_of_rounds=3200, transient_phase_length=100, rho=0.8)
+#     simulator = analyzer.get_simulator()
+#     c2 = 0
+#     consecutive = 0
+#     consecutive_between = 0
+#     sizes = []
+#     sizes_between = []
+#     lowest_variance = 10
+#     for r in range(10):
+#         res = simulator.simulate_FCFS(100000)
+#         last = []
+#         var_sum = 0
+#         c = 0
+#         last_was_less_than_lowest_variance = False
+#         for i in res[1]:
+#             last.append(i)
+#             if len(last) > 1000:
+#                 last.pop(0)
+#                 mean = sum(last) / 1000
+#                 for e in last:
+#                     var_sum += (e - mean) ** 2
+#                 variance = var_sum / (1000 - 1)
+#                 var_sum = 0
+#                 if variance <= lowest_variance:
+#                     if last_was_less_than_lowest_variance:
+#                         consecutive += 1
+#                     else:
+#                         if consecutive_between > 0:
+#                             sizes_between.append(consecutive_between)
+#                             consecutive_between = 0
+#                     last_was_less_than_lowest_variance = True
+#                 else:
+#                     if last_was_less_than_lowest_variance:
+#                         if consecutive > 0:
+#                             c2 += 1
+#                             sizes.append(consecutive)
+#                             consecutive = 0
+#                     else:
+#                         consecutive_between += 1
+#                     last_was_less_than_lowest_variance = False
+#
+#             c += 1
+#         consecutive = 0
+#         consecutive_between = 0
+#         if len(sizes) > 0:
+#             print("count: " + str(c2) + "  size mean: " + str(sum(sizes) / len(sizes)))
+#         else:
+#             print("no equilibrium this time")
+#         if len(sizes_between) > 0:
+#             print("size between mean: ", sum(sizes_between) / len(sizes_between))
+#         elif not (len(sizes_between) > 0) and not (len(sizes) > 0):
+#             print("no equilibrium this time")
+#         else:
+#             print("always on equilibrium")  # this will probably never happen
+#         sizes = []
+#         sizes_between = []
+#         c2 = 0
