edb5e3658d612636b745c785e7b69b7421bbc0ab
gsbevilaqua <gs_bevilaqua@hotmail.com>
2018-12-03 23:27:57 -0200

    added variances graphs

--- Web.py
+++ Web.py
@@ -7,36 +7,33 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    discipline = None
-    rho = None
-    if request.method == 'POST':
-        if request.form['discipline'] == 'fcfs':
-            discipline = 1
-        elif request.form['discipline'] == 'lcfs':
-            discipline = 2
-        if request.form['util'] == 'util2':
-            rho = 0.2
-        elif request.form['util'] == 'util4':
-            rho = 0.4
-        elif request.form['util'] == 'util6':
-            rho = 0.6
-        elif request.form['util'] == 'util8':
-            rho = 0.8
-        elif request.form['util'] == 'util9':
-            rho = 0.9
-        return redirect(url_for('simul', discipline=discipline, rho=rho))
+	discipline = None
+	rho = None
+	if request.method == 'POST':
+		if request.form['discipline'] == 'fcfs':
+			discipline = 1
+		elif request.form['discipline'] == 'lcfs':
+			discipline = 2
+		if request.form['util'] == 'util2':
+			rho = 0.2
+		elif request.form['util'] == 'util4':
+			rho = 0.4
+		elif request.form['util'] == 'util6':
+			rho = 0.6
+		elif request.form['util'] == 'util8':
+			rho = 0.8
+		elif request.form['util'] == 'util9':
+			rho = 0.9 
+		return redirect(url_for('simul', discipline=discipline, rho=rho))
 
-    elif request.method == 'GET':
-        return render_template("home.html")
-
+	elif request.method == 'GET':
+		return render_template("home.html")
 
 @app.route("/simul/disc<int:discipline>util<float:rho>")
 def simul(discipline, rho):
-    master = Master()
-    results_w, results_nq = master.webmain(discipline, rho)
-    return render_template("simulation.html", discipline=discipline, rho=rho, results_w=json.dumps(results_w),
-                           results_nq=json.dumps(results_nq))
-
+	master = Master()
+	results_w, results_w_vars, results_nq, results_nq_vars = master.webmain(discipline, rho)
+	return render_template("simulation.html", discipline=discipline, rho=rho, results_w=json.dumps(results_w), results_w_vars=json.dumps(results_w_vars), results_nq=json.dumps(results_nq), results_nq_vars=json.dumps(results_nq_vars))
 
 if __name__ == "__main__":
     app.run(debug=True)
