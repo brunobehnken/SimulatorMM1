1f79a361ac9fb0889d30ecf50cefb6836c6fbf57
gsbevilaqua <gs_bevilaqua@hotmail.com>
2018-12-18 14:48:57 -0200

    Update Web.py

--- Web.py
+++ Web.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 def home():
     discipline = None
     rho = None
+    k = 1_280
+    seed = "-1"
     if request.method == 'POST':
         if request.form['discipline'] == 'fcfs':
             discipline = 1
@@ -25,25 +27,32 @@ def home():
         elif request.form['util'] == 'util9':
             rho = 0.9
 
-        # TODO must check if 'round_size' and seed were informed. If not, default values are 1_280 and None.
-        k = 1_280  # TODO must implement 'round_size' request box on front-end. This line is a place-holder.
-        seed = None  # TODO must implement seed request box on front-end. This line is a place-holder.
-        # return redirect(url_for('simul', discipline=discipline, rho=rho, seed=seed, k=k))
-        return redirect(url_for('simul', discipline=discipline, rho=rho))
+        if(request.form['k'] is not ''):
+          try:
+            k = int(request.form['k'])
+          except:
+            pass
+        if(request.form['seed'] is not ''):          
+          try:
+            seed = int(request.form['seed'])
+          except:
+            pass
+        return redirect(url_for('simul', discipline=discipline, rho=rho, k=k, seed=seed))
 
     elif request.method == 'GET':
         return render_template("home.html")
 
 
-@app.route("/simul/disc<int:discipline>util<float:rho>")
-# def simul(discipline, rho, seed, k):
-def simul(discipline, rho):
+@app.route("/simul/d<int:discipline>u<float:rho>k<int:k>s<string:seed>")
+def simul(discipline, rho, k, seed):
+    if seed == "-1":
+      seed = None
     master = Master()
     results_w, results_w_icl, results_w_icu, \
         results_w_vars, results_w_vars_icl, results_w_vars_icu, \
         results_nq, results_nq_icl, results_nq_icu, \
         results_nq_vars, results_nq_vars_icl, results_nq_vars_icu, \
-        execution_time = master.webmain(discipline, rho)  # TODO pass k and seed as arguments
+        execution_time = master.webmain(discipline, rho, k, seed)
     execution_time = "Execution time: {0:.2f} seconds".format(execution_time)
     discipline = "FCFS" if discipline == 1 else "LCFS"
     return render_template("simulation.html", discipline=discipline, rho=rho,
